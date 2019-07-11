5a4b47bfe4bf3eb79508d17626cace2068b26bff
DalkommBear <inin0119@naver.com>
2019-07-11 17:31:44 +0900

    create StackNavigation & Add ResultScreen

--- App.js
+++ App.js
@@ -1,97 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor : 'white' }}>
+        <Text style = {{fontSize : 20, marginBottom : 30}}>메인화면</Text>
+        <Button
+          title = "차량 등록"
+          color = '#8a2be2'
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Hello World</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+        <Button
+          title = '차량 조회'
+          color = '#8a2be2'
+          onPress = {() => this.props.navigation.navigate('Search')}
+          />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', backgroundColor : 'white' }}>
+        <Text style = {{fontSize : 20, marginBottom : 30, marginTop : 15}}>차량 등록</Text>
+
+        <Button
+          color = '#8a2be2'
+          title="확인"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          color = '#8a2be2'
+          title="취소"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+class SearchScreen extends React.Component {
+  render () {
+    return (
+      <View style = {styles.container}>
+      <View style = {{flex : 1, alignItems : 'center'}}>
+        <Text style = {{fontSize : 20, marginBottom : 30, marginTop : 15}}>차량 조회</Text>
+        <View stlye = {styles.content}>
+        <Button
+          color = '#8a2be2'
+          title="확인"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+
+        <Button
+          color = '#8a2be2'
+          title="취소"
+          onPress={() => this.props.navigation.goBack()}
+        />
+        </View>
+      </View>
+      </View>
+    )
+  }
+}
+
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+
+    Search : {
+      screen : SearchScreen
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container : {
+    flex : 1,
+    alignItems : 'center',
+    backgroundColor : 'lightpink'
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
+  content : {
+    alignItems : 'center',
+    marginBottom : 30
+  }
+
 });
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
