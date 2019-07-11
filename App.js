7cc25788d7b571afa97d65cb1f812bb256ceaeb9
kdha123 <012rlaehdgus@naver.com>
2019-07-11 17:28:56 +0900

    test

--- App.js
+++ App.js
@@ -1,13 +1,101 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import Routes from './Routes.js'
+import React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
-class reactTutorialApp extends Component {
-   render() {
-      return (
-         <Routes />
-      )
-   }
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.box}>Test</Text>
+        <Button
+          title="조회하기"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
 }
-export default reactTutorialApp
-AppRegistry.registerComponent('reactTutorialApp', () => reactTutorialApp)
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('MoreDetails')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+class MoreDetailsScreen extends React.Component {
+   render() {
+     return (
+       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Details Screen~~~</Text>
+         <Button
+           title="Go to Details... again"
+           onPress={() => this.props.navigation.navigate('Details')}
+         />
+         <Button
+           title="Go to Home"
+           onPress={() => this.props.navigation.navigate('Home')}
+         />
+         <Button
+           title="Go back"
+           onPress={() => this.props.navigation.goBack()}
+         />
+       </View>
+     );
+   }
+ }
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+    MoreDetails: {
+       screen: MoreDetailsScreen,
+    }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     marginTop: 20,
+     backgroundColor: 'skyblue',
+   },
+   font: {
+     fontSize: 20
+   },
+   box: {
+   backgroundColor: "white",
+   fontSize: 20
+   }
+ });
+ 
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import HomeScreen from './App';
+
+export default class test extends Component {
+    render () {
+        return (
+            <HomeScreen text='Good morning everyone'></HomeScreen>
+        );
+    }
+}
